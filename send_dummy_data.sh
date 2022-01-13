05435688621b02cc2b11ba1b17e52289ea3ff4d0
leighleighleigh <leighcharlesoliver@gmail.com>
2022-01-14 07:48:25 +1100

    Screencap updated from new dummy data

--- send_dummy_data.sh
+++ send_dummy_data.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/bash
-for p in {0..65535}
+for i in {0..65535}
 do
-	v1=$(echo "$p + s($p-35000)*2000" | bc -l)
+	# yes this is trigonometric functions in BASH
+	# I didnt know this was possible
+	v1=$(echo "s(2*3.1415*$i/15535)*60000" | bc -l)
 	v=$(printf "%.0f" $v1)
+        p1=$(echo "s(2*3.1415*$i/25535)*20000" | bc -l)
+        p=$(printf "%.0f" $p1)
 	BASECMD="cansend vcan0 450#"
 	VAL=$(printf "%04X" $p)
 	VAL2=$(printf "%04X" $v)

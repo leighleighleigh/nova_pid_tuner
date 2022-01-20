0a0ee529c1564ec86db57d80aac916ad5c50c27b
leighleighleigh <leighcharlesoliver@gmail.com>
2022-01-20 20:09:11 +1100

    Update install_requires

--- setup.py
+++ setup.py
@@ -20,6 +20,16 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+        'aenum==3.1.5',
+        'numpy==1.22.0',
+        'PyQt5==5.15.6',
+        'PyQt5-Qt5==5.15.2',
+        'PyQt5-sip==12.9.0',
+        'pyqtgraph==0.12.3',
+        'python-can==3.3.4',
+        'wrapt==1.13.3'
+    ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.8",
